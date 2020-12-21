c14d4b8242b63c13fe5b067e3602f2c7fa49e18f
Naimur Rahman <zeeem13@gmail.com>
2020-12-21 12:14:56 -0500

    replicated ficility POST error

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
 import 'dart:collection';
 import 'package:http/http.dart' as http;
 import 'NetworkGateway/networkManager.dart';
+import 'dart:convert';
 
-String baseAPIurl = "https://yizhouzhao.dev";
+String baseAPIurl = "yizhouzhao.dev";
 
 main(List<String> args) async {
   print("dart running --->>");
@@ -16,15 +17,17 @@ api_call_GET() async {
   NetworkManager apiNetworkManager =
       NetworkManager(baseAPIurl, nursingHome: false);
   var response = await apiNetworkManager.request('GET', '/api/shortdoctors/');
-  print(response);
-  print(response.body);
+  var body = await response.body;
+  var valMap = await jsonDecode(response.body);
+  // print(response);
+  print(body);
   print(response.statusCode);
-  await print(response.body);
+  // await print(response.body);
 }
 
 api_call_POST() async {
-  String userNum = '3';
-  String usertype = '0';
+  String userNum = '2';
+  String usertype = '1';
   Map<String, dynamic> map = {
     "username": "test_u${userNum}",
     "email": "test_u${userNum}@email.com",
@@ -40,14 +43,29 @@ api_call_POST() async {
     "last_name": "ln_test_user$userNum"
   };
 
-  print('map body====>> $map');
+  Map<String, dynamic> loginMap = {"username": "test_u2", "password": "123123"};
+
+  // print('map body====>> $map');
 
   NetworkManager apiNetworkManager =
       NetworkManager(baseAPIurl, nursingHome: false);
   var response =
-      await apiNetworkManager.request('POST', '/api/shortdoctors/', body: map);
-  print(response);
-  print(response.body);
-  print(response.statusCode);
-  await print(response.body);
+      await apiNetworkManager.request('POST', '/api/login/', body: loginMap);
+
+  var body = jsonDecode(response.body);
+
+  print('logged in as: ${body['id']}');
+
+  Map<String, dynamic> facilityAssignMap = {"user": "${body['id']}"};
+
+// facility uuid: 381c31f1-f4fc-4d9a-aac0-6cdf7a78cdda
+
+  var response2 = await apiNetworkManager.request(
+      'POST', '/api/belongsto/381c31f1-f4fc-4d9a-aac0-6cdf7a78cdda/',
+      body: facilityAssignMap);
+
+  // print(response);
+  // print(response2.body);
+  print(response2.statusCode);
+  // await print(response.body);
 }
